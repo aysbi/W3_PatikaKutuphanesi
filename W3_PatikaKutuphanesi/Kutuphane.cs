1c4b6750f8531dfc5e1cca12e2cb35068702523a
Aysel Bilmez <aybilmez@gmail.com>
2024-10-23 13:37:48 +0300

    Proje dosyası ekle.

---
+++ W3_PatikaKutuphanesi/Kutuphane.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3_PatikaKutuphanesi
+{
+    public class Kutuphane //class olusturuyoruz
+    {
+        public string KitapAdi {  get; set; } //property olusturduk
+        public string YazarAdi { get; set; }
+        public string YazarSoyadi { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Yayinevi { get; set; }
+        public DateTime YayinTarihi; //bu da field yani alan olusturduk 
+
+        public Kutuphane() //bu bir parametre almayan constructor
+        {
+            YayinTarihi = DateTime.Today;
+        }
+
+        public Kutuphane(string _kitapAdi, string _yazarAdi, string _yazarSoyadi, int _sayfaSayisi, string _yayinevi) 
+            //bu parametre alan bir constructor
+        {
+            _kitapAdi = KitapAdi;
+            _yazarAdi = YazarAdi;
+            _yazarSoyadi = YazarSoyadi;
+            _sayfaSayisi = SayfaSayisi;
+            _yayinevi = Yayinevi;
+            YayinTarihi = DateTime.Today;
+
+        }
+
+        public void BilgiYazdir() //bu bir metot 
+        {
+            Console.WriteLine($"Kitap Adi = {KitapAdi} \r\nYazar = {YazarAdi} {YazarSoyadi} \r\nYayinevi = {Yayinevi} \r\nSayfa Sayisi = {SayfaSayisi}" +
+                $"\r\nYayin Tarihi = {YayinTarihi.ToShortDateString()} \r\n------------------------------- \r\n");
+        }
+
+
+    }
+}
---
+++ W3_PatikaKutuphanesi/Program.cs
@@ -0,0 +1,14 @@
+﻿using W3_PatikaKutuphanesi;
+
+Kutuphane Kitap1 = new Kutuphane("Bir Kitap", "Ahmet", "Yilmaz", 320, "Alfa Yayinlari");
+//parametre alan cosntructorlarda nesneleri tek satirda atayabiliriz
+
+Kutuphane Kitap2 = new Kutuphane(); //burada parametre almayan constructor kullandigimiz icin tek tek nesneleri atamaliyiz
+Kitap2.KitapAdi = "Baska Bir Kitap";
+Kitap2.YazarAdi = "Ayse";
+Kitap2.YazarSoyadi = "Saran";
+Kitap2.SayfaSayisi = 215;
+Kitap2.Yayinevi = "Beta Yayinevi ";
+
+Kitap1.BilgiYazdir();
+Kitap2 .BilgiYazdir();
